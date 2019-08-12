1be9f2d8d6f5ead1de3d0641692896f0538d5da1
belcortes <belcortes@gmail.com>
2019-08-12 09:13:17 -0700

    removing unecessary comments

--- src/api-helpers.js
+++ src/api-helpers.js
@@ -10,16 +10,4 @@ export const fetchSingleQuote = async quoteId => {
 export const fetchQuotes = async (searchBy, search, filter) => {
   const res = await axios.get(`${URL_BASE}?sortBy=${filter}&${searchBy}=${search}`);
   return res.data.results;
-  // https://auth0-exercise-quotes-api.herokuapp.com/api/quotes?sortBy=text&text=fire
-  // https://auth0-exercise-quotes-api.herokuapp.com/api/quotes?text=fire?sortBy=text
 };
-
-// export const fetchSearchedQuotes = async (search, searchBy) => {
-//   const res = await axios.get(`${URL_BASE}?${searchBy}=${search}`);
-//   return res.data.results;
-// };
-//
-// export const fetchSortedQuotes = async filter => {
-//   const res = await axios.get(`${URL_BASE}?sortBy=${filter}`);
-//   return res.data.results;
-// };
