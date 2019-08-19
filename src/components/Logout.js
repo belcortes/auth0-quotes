6c5b091f119a1df3577ba08ef87811e822c34c5d
belcortes <belcortes@gmail.com>
2019-08-19 16:44:50 -0700

    managing production redirects with env variables

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -4,11 +4,12 @@ import { useAuth0 } from '../react-auth0-wrapper';
 
 const Logout = () => {
   const { isAuthenticated, logout } = useAuth0();
+  const appUrl = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD_URL : process.env.REACT_APP_DEV_URL;
   return (
     <>
     {isAuthenticated && (
       <Link
-        to={`https://${process.env.REACT_APP_DOMAIN}/v2/logout?returnTo=http%3A%2F%2Fhttp://localhost:3000&client_id=${process.env.REACT_APP_CLIENT_ID}`}
+        to={`https://${process.env.REACT_APP_DOMAIN}/v2/logout?returnTo=http%3A%2F%2F${appUrl}&client_id=${process.env.REACT_APP_CLIENT_ID}`}
         className="navbar_button"
         onClick={() => logout()}
       >
