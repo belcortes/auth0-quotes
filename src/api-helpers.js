a4181915bb405971a04ee7763c46d36a80421fc3
belcortes <belcortes@gmail.com>
2019-08-20 10:56:04 -0700

    api calls error handling

--- src/api-helpers.js
+++ src/api-helpers.js
@@ -1,12 +1,20 @@
 import axios from 'axios';
 
 export const fetchSingleQuote = async quoteId => {
-  const res = await axios.get(`${process.env.REACT_APP_URL_BASE}/${quoteId}`);
-  return res.data;
+  try {
+    const res = await axios.get(`${process.env.REACT_APP_URL_BASE}/${quoteId}`);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const fetchQuotes = async (searchBy, search, sort, page) => {
   const searchQuery = search ? `&${searchBy}=${search}` : '';
-  const res = await axios.get(`${process.env.REACT_APP_URL_BASE}?sortBy=${sort}${searchQuery}&page=${page}`);
-  return res.data.results;
+  try {
+    const res = await axios.get(`${process.env.REACT_APP_URL_BASE}?sortBy=${sort}${searchQuery}&page=${page}`);
+    return res.data.results;
+  } catch (error) {
+    console.log(error);
+  }
 };
